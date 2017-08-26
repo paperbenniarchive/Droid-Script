7be9d23106c77bfe5b4ca7291fd0b12ecff1c732
Paperbenni <paperbenni@gmail.com>
2017-08-26 14:20:03 +0200

    test

--- encrypter/encrypter.js
+++ encrypter/encrypter.js
@@ -4,7 +4,7 @@ function OnStart()
 {
     //Create a layout with objects vertically centered.
     lay = app.CreateLayout( "linear", "VCenter,FillXY" );    
-
+    lay.SetBackColor( "WHITE" );
     //Create an text edit box for password.
     edtPass = app.CreateTextEdit( "myPass1", 0.4 );
     lay.AddChild( edtPass );
@@ -12,22 +12,30 @@ function OnStart()
     //Create an text edit box for message
     edt = app.CreateTextEdit( "Hello World!", 0.8, 0.3 );
     edt.SetMargins( 0,0.1,0,0 );
+    edt.SetTextColor( "BLUE" );
+    edt.SetCursorColor("#352B48"  );
     lay.AddChild( edt );
     
     //Create an 'Encrypt' button..
-    btnEncrypt = app.CreateButton( "Encrypt", 0.3 );
+    btnEncrypt = app.CreateButton( "Encrypt", 0.7, 0.1, "Custom");
     btnEncrypt.SetOnTouch( btnEncrypt_OnTouch );
     lay.AddChild( btnEncrypt );
 
     //Create an 'Decrypt' button..
-    btnDecrypt = app.CreateButton( "Decrypt", 0.3 );
+    btnDecrypt = app.CreateButton( "Decrypt", 0.7, 0.1, "Custom" );
     btnDecrypt.SetOnTouch( btnDecrypt_OnTouch );
     lay.AddChild( btnDecrypt );
     
     //Create a 'Hash' button..
-    btnHash = app.CreateButton( "Hash", 0.3 );
+    btnHash = app.CreateButton( "Hash", 0.7, 0.1, "Custom" );
     btnHash.SetOnTouch( btnHash_OnTouch );
     lay.AddChild( btnHash );
+    
+    btnCopy = app.CreateButton( "Copy to clipboard", 0.7, -1, "Custom");
+  //  btnCopy.SetStyle( "#4285F4", "#4285F4", 4 );
+    btnCopy.SetOnTouch( btnCopy_OnTouch );
+    lay.AddChild( btnCopy );
+    
     
     //Add layout to app.    
     app.AddLayout( lay );
@@ -67,3 +75,8 @@ function btnHash_OnTouch()
     var md5 = crypt.Hash( pass, "MD5"); 
     app.ShowPopup( md5 );
 }
+
+
+function btnCopy_OnTouch(){
+	app.SetClipboardText(	edt.GetText()  );
+	}
