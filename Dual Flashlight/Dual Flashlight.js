fc4211cd55d93956b63f5075ad40ab271e61d928
Paperbenni <paperbenni@gmail.com>
2017-08-27 21:46:07 +0200

    added the dual flashlight app

---
+++ Dual Flashlight/Dual Flashlight.js
@@ -0,0 +1,13 @@
+function OnStart()
+{
+	lay = app.CreateLayout( "linear", "VCenter,FillXY" );	
+lay.SetBackColor( "WHITE" );
+cam = app.CreateCameraView(1,1 );
+laytop = app.CreateLayout( "Linear", "VCenter,FillXY" );
+laytop.SetBackColor( "WHITE" );
+ lay.AddChild( cam );
+	app.AddLayout( lay );
+app.AddLayout( laytop );
+	cam.SetFlash( true );
+	app.SetScreenBrightness( 1 );
+}
