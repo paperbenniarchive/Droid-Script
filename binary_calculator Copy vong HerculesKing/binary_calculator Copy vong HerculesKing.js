e423a0a32e442e8594a8f5ba1f660a8256f1079c
M0nti <rentata1337@gmail.com>
2017-09-03 18:08:17 +0200

    Kuddelmuddel vong Johannes gecleaned

--- binary_calculator Copy vong HerculesKing/binary_calculator Copy vong HerculesKing.js
+++ binary_calculator Copy vong HerculesKing/binary_calculator Copy vong HerculesKing.js
@@ -22,39 +22,39 @@ function OnStart()
   lay3 = app.CreateLayout("Linear", "Horizontal, FillX");
   lay3.SetBackColor("white");
   lay.AddChild(lay3);
-  lay3.SetMargins( 0.01, 0.4, 0.01, 0.01 );
+  lay3.SetMargins( 0, 0.4, 0, 0.02 );
   
   lay4 = app.CreateLayout("Linear", "Horizontal, FillX");
   lay1.SetBackColor("white");
   lay.AddChild(lay4);
  
-	txtbin =app.CreateText( sum );
-	txtbin.SetMargins( 0, 0.1, 0.01, 0.01 );
-	txtnum =app.CreateText(suk);
-	txtnum.SetMargins( 0, 0.1, 0.01, 0.01 );
-
-
-	lay1.AddChild(txtbin  );
-	lay2.AddChild( txtnum );
-
-
-	b0 = app.CreateButton( "0", 0.7, -1, "Custom" );
-  b0.SetStyle( "green", "#4285F4", 90000,"black", 0.7 , 0 );
-  b0.SetTextColor( "black" );
-  lay3.AddChild( b0 );
-  b0.SetOnTouch( b0_OnTouch );
+	txtBin =app.CreateText( sum );
+	txtBin.SetMargins( 0, 0.1, 0.01, 0.01 );
+	txtBin.SetTextColor( "black" );
+	txtNum =app.CreateText(suk);
+	txtNum.SetMargins( 0, 0.1, 0.01, 0.01 );
+	txtNum.SetTextColor( "black" );
+	
+	lay1.AddChild(txtBin  );
+	lay2.AddChild( txtNum );
+	
+	btn0 = app.CreateButton( "0", 0.8, -1, "Custom" );
+  btn0.SetStyle( "green", "#4285F4", 90000,"black", 0.7 , 0 );
+  btn0.SetTextColor( "black" );
+  lay3.AddChild( btn0 );
+  btn0.SetOnTouch( btn0_OnTouch );
   
-	b1 = app.CreateButton( "1", 0.8, -1, "Custom" );
-  b1.SetStyle( "green", "#4285F4", 90000,"black", 0.7 , 0 );
-  b1.SetTextColor( "black" );
-  lay3.AddChild( b1);
-  b1.SetOnTouch( b1_OnTouch );
+	btn1 = app.CreateButton( "1", 0.8, -1, "Custom" );
+  btn1.SetStyle( "green", "#4285F4", 90000,"black", 0.7 , 0 );
+  btn1.SetTextColor( "black" );
+  lay3.AddChild( btn1);
+  btn1.SetOnTouch( btn1_OnTouch );
   
-	bb = app.CreateButton( "calc", 0.8, -1, "Custom" );
-  bb.SetStyle( "green", "#4285F4", 90000,"black", 0.7 , 0 );
-  bb.SetTextColor( "black" );
-  lay4.AddChild( bb);
-  bb.SetOnTouch( bb_OnTouch );
+	btnClc = app.CreateButton( "=", 0.8, -1, "Custom" );
+  btnClc.SetStyle( "green", "#4285F4", 90000,"black", 0.7 , 0 );
+  btnClc.SetTextColor( "black" );
+  lay4.AddChild( btnClc);
+  btnClc.SetOnTouch( btnClc_OnTouch );
   
   btnC = app.CreateButton( "C", 0.8, -1, "Custom" );
   btnC.SetStyle( "green", "#4285F4", 90000, "black", 0.7, 0 );
@@ -66,53 +66,56 @@ function OnStart()
 function btnC_OnTouch()
 {
 	sum="";
-	txtbin.SetText( sum );
+	txtBin.SetText( sum );
 	
 	suk="";
-	txtnum.SetText( sum );
+	txtNum.SetText( sum );
 }
 
-function b0_OnTouch() {
+function btn0_OnTouch() 
+{
   sum += "0";
-  txtbin.SetText(sum);
+  txtBin.SetText(sum);
 }
 
-function b1_OnTouch() {
+function btn1_OnTouch() 
+{
   sum += "1";
-  txtbin.SetText(sum);
+  txtBin.SetText(sum);
 }
 
-function bb_OnTouch()
-{  suk = 0
+function btnClc_OnTouch()
+{
+	suk = 0
 	 y = 0
 
-var bam = sum
-var zahl =""
-zahl -=3
-zahl +=3
+	var bam = sum
+	var zahl =""
+	zahl -=3
+	zahl +=3
 
-x = zahl % 1
-zahl += (bam  - x) / 1 *  2
-zahl = Math.pow( zahl  , y)
-x ++
-y++ 
-	 suk = zahl
+	x = zahl % 1
+	zahl += (bam  - x) / 1 *  2
+	zahl = Math.pow( zahl  , y)
+	x ++
+	y++ 
+	suk = zahl
 	 
-	 check = ""
+	check = ""
 	  
-	 if (sum > 99)check = sum% 1000
-	 if (check == "1") suk += 8
+	if (sum > 99)check = sum% 1000
+	if (check == "1") suk += 8
 	 
 	 
-	 check = sum% 10000
-	 if (check == "1") suk += 16
-	 check = sum% 100000
-	 if (check == "1") suk += 32
-	 check = sum% 1000000
-	 if (check == "1") suk += 64
-	 check = sum% 10000000
-	 if (check == "1") suk += 128
-	 check = sum% 100000000
-	 if (check == "1") suk += 256
-	 txtnum.SetText( suk ); 
+	check = sum% 10000
+	if (check == "1") suk += 16
+	check = sum% 100000
+	if (check == "1") suk += 32
+	check = sum% 1000000
+	if (check == "1") suk += 64
+	check = sum% 10000000
+	if (check == "1") suk += 128
+	check = sum% 100000000
+	if (check == "1") suk += 256
+	txtNum.SetText( suk ); 
 }
